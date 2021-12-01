7c2ee7ef48799e571f46ef30ba9dbc8de6efc30d
ZakariaBOUKHRISS <zakaria.boukhriss@ump.ac.ma>
2021-12-01 02:58:47 +0100

    second commit

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ with open('file.json', 'r') as myfile:
 def index():
     return render_template('index.html', title="page", jsonfile=json.dumps(data))
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port)
